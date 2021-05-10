fe9a9dac6a0a57cb2882468ac5bf2bb12995bc16
mdx97 <mathewhorner456@gmail.com>
2021-05-09 23:18:05 -0500

    Add some anti-aliasing. FOV is a bit weird atm.

--- src/main.rs
+++ src/main.rs
@@ -13,31 +13,37 @@ use std::fs::File;
 use vec3::Vec3;
 
 // Dimensions of the output image, in pixels.
-const WIDTH: usize = 640;
-const HEIGHT: usize = 480;
+const WIDTH: usize = 1920;
+const HEIGHT: usize = 1080;
 
 // Distance between the image plane and the vantage point.
-const VANTAGE_DISTANCE: f64 = 50.0;
+const VANTAGE_DISTANCE: f64 = 100.0;
+
+// Determines the quality of anti-aliasing.
+const AA_SCALE: usize = 4;
 
 fn main() {
-    let mut image: RgbImage = ImageBuffer::new(WIDTH as u32, HEIGHT as u32);
+    let buffer_width = (WIDTH * AA_SCALE) as u32;
+    let buffer_height = (HEIGHT * AA_SCALE) as u32;
+    let mut image: RgbImage = ImageBuffer::new(buffer_width, buffer_height);
     let origin = Vec3::new(0.0, 0.0, 0.0);
+    //let light = Vec3::new(0.0, 200.0, 50.0);
     let scene_objects = vec![
-        Sphere::new(Vec3::new(-100.0, 0.0, 50.0), 25),
-        Sphere::new(Vec3::new(-50.0, 0.0, 50.0), 25),
-        Sphere::new(Vec3::new(0.0, 0.0, 50.0), 25),
-        Sphere::new(Vec3::new(50.0, 0.0, 50.0), 25),
-        Sphere::new(Vec3::new(100.0, 0.0, 50.0), 25),
+        Sphere::new(Vec3::new(-200.0, 0.0, -50.0), 25),
+        Sphere::new(Vec3::new(-100.0, 0.0, -50.0), 25),
+        Sphere::new(Vec3::new(0.0, 0.0, -50.0), 25),
+        Sphere::new(Vec3::new(100.0, 0.0, -50.0), 25),
+        Sphere::new(Vec3::new(200.0, 0.0, -50.0), 25),
     ];
 
-    for i in 0..HEIGHT {
-        for j in 0..WIDTH {
+    for i in 0..buffer_height {
+        for j in 0..buffer_width {
             // Cast a ray from the vantage point to the center of the current pixel.
             let primary_ray = Ray::new(
                 origin.clone(),
                 Vec3::new(
-                    -((WIDTH / 2) as f64) + j as f64,
-                    ((HEIGHT / 2) as f64) - i as f64,
+                    -((buffer_width / 2) as f64) + j as f64,
+                    ((buffer_height / 2) as f64) - i as f64,
                     VANTAGE_DISTANCE
                 ),
             );
@@ -66,8 +72,29 @@ fn main() {
         }
     }
 
+    // Use supersampling to provide Anti-Aliasing.
+    let mut final_image: RgbImage = ImageBuffer::new(WIDTH as u32, HEIGHT as u32);
+    for i in 0..HEIGHT {
+        for j in 0..WIDTH {
+            let mut color_totals = [0.0, 0.0, 0.0];
+            for k in 0..AA_SCALE {
+                for l in 0..AA_SCALE {
+                    let pixel = image.get_pixel(((j * AA_SCALE) + l) as u32, ((i * AA_SCALE) + k) as u32);
+                    color_totals[0] += pixel[0] as f64;
+                    color_totals[1] += pixel[1] as f64;
+                    color_totals[2] += pixel[2] as f64;
+                }
+            }
+            final_image.put_pixel(j as u32, i as u32, Rgb([
+                (color_totals[0] / AA_SCALE.pow(2) as f64) as u8,
+                (color_totals[1] / AA_SCALE.pow(2) as f64) as u8,
+                (color_totals[2] / AA_SCALE.pow(2) as f64) as u8,
+            ]));
+        }
+    }
+
     let mut file = File::create("render.jpg").unwrap();
     JpegEncoder::new(&mut file)
-        .encode_image(&image)
+        .encode_image(&final_image)
         .unwrap();
 }
